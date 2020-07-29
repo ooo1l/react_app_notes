c94dcea9a5a7a860870f88cc16e01f36565703b4
ooo1l <ccc_1l@qq.com>
2020-07-29 22:47:44 +0800

    2-3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const handleAddNote = (event) => {
     event.preventDefault()
@@ -21,16 +22,25 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
       <form onSubmit={handleAddNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
